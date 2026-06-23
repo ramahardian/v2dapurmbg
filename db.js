8ded4241c32bf58b0641f0ad47c1b8ea8875812d
ramahardian <ramahardianstudio@gmail.com>
2026-06-23 11:08:35 +0700

    l

--- db.js
+++ db.js
@@ -4,9 +4,9 @@ require('dotenv').config();
 const pool = mysql.createPool({
   host: process.env.DB_HOST ||  'localhost',
   port: process.env.DB_PORT || 3306,
-  user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASSWORD || '',
-  database: process.env.DB_NAME || 'mbg_kitchen' ,
+  user: process.env.DB_USER || 'u778324865_dapur',
+  password: process.env.DB_PASSWORD || '#dH9lSk6S:Fx',
+  database: process.env.DB_NAME || 'u778324865_dapur' ,
   waitForConnections: true,
   connectionLimit: 10,
   decimalNumbers: true,
