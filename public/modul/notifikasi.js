25e6f757dc3433d903d97403902def92c0c0c88f
ramahardian <ramahardianstudio@gmail.com>
2026-07-27 22:55:41 +0700

    l

--- public/modul/notifikasi.js
+++ public/modul/notifikasi.js
@@ -513,8 +513,4 @@ async function notifRefresh() {
 }
 
 // ===== Utility =====
-
-function getInitials(name) {
-  if (!name) return '?';
-  return name.split(' ').map(w => w[0]).join('').slice(0, 2).toUpperCase();
-}
+// getInitials from utilitas.js
